772f6ef4680a8cfed016974064aed07a4d6994a6
amagmail <skynet@skynet.ru>
2024-11-08 20:27:16 +0300

    SPRINT-6. Исправления по проверке CheckStyle

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -15,29 +15,43 @@ public interface TaskManager {
     // Редактирование задач
     // *******************************************************
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     // *******************************************************
     // Получение задач
     // *******************************************************
     List<Task> getTasks();
+
     List<Epic> getEpics();
+
     List<Subtask> getSubtasks();
+
     Task getTaskByID(int id);
+
     Epic getEpicByID(int id);
+
     Subtask getSubtaskByID(int id);
+
     List<Subtask> getEpicSubtasksByID(int id);
+
     List<Task> getHistory();
 
     // *******************************************************
     // Удаление задач
     // *******************************************************
     void removeTasks();
+
     void removeEpics();
+
     void removeSubtasks();
+
     void removeTaskByID(int id);
+
     void removeEpicByID(int id);
+
     void removeSubtaskByID(int id);
 
 }
